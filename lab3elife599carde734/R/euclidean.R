14ddd780bd0383613eb65552d9e10cd3886c237a
Elisio Quintino <elife599@student.liu.se>
2021-09-08 22:04:36 +0200

    Version 0.1 of package with euclidean completely working, but rest in poor shape. this is just a test!

---
+++ lab3elife599carde734/R/euclidean.R
@@ -0,0 +1,9 @@
+euclidean <-
+function(a, b){
+  stopifnot((a%%1) + (b%%1) == 0) #check if both numers are integers (even if their R type is double!!!)
+  if (b == 0){
+    return(max(a,-a))
+  } else {
+    return(euclidean(b, a%%b))
+  }
+}
