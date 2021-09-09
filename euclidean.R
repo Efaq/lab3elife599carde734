7bbea5f6be9e79ef4dd077e3a841e1e5fdfe80a2
Elisio Quintino <elife599@student.liu.se>
2021-09-09 11:53:12 +0200

    Some updates: 1) Documentation for euclidean now updated by roxygen2 2) euclidean.R from outside the package deleted, the one inside the package will be the working version now. 3) Some description updated.

--- euclidean.R
+++
@@ -1,8 +0,0 @@
-euclidean = function(a, b) {
-  stopifnot((a %% 1) + (b %% 1) == 0) #check if both numbers are integers (even if their R type is double!!!)
-  if (b == 0) {
-    return(max(a, -a))
-  } else {
-    return(euclidean(b, a %% b))
-  }
-}
--- lab3elife599carde734/R/euclidean.R
+++ lab3elife599carde734/R/euclidean.R
@@ -1,3 +1,4 @@
+#' This is me testing roxygen documentation
 euclidean <-
 function(a, b){
   stopifnot((a%%1) + (b%%1) == 0) #check if both numers are integers (even if their R type is double!!!)
