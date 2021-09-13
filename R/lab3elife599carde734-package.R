29c03a46f48d1b5973a34eca78b79d29c7b3d45d
Elisio Quintino <elife599@student.liu.se>
2021-09-13 13:28:18 +0200

    updating description

--- R/lab3elife599carde734-package.R
+++ R/lab3elife599carde734-package.R
@@ -1,4 +1,6 @@
 #' @details
-#' Package for lab3 containing functions euclidean and dijkstra
+#' This package contains functions and dataset for Assignment 3 in Advanced R.
+#' It contains two functions, euclidean and dijkstra, as well as the dataset wiki_graph.
+#' Read more about them in the respective documentations.
 #' @keywords internal
 "_PACKAGE"
